18d9b5097eb3adfdf45f7e086aa1d200e7dfe8a7
jollaman999 <admin@jollaman999.com>
2014-09-29 19:51:51 +0900

    Multirom Fix
    Change-Id: I708bbfe2d71b8ebd72051c51d4163cccb103eec0

--- mr_init_devices.c
+++ mr_init_devices.c
@@ -14,9 +14,9 @@ const char *mr_init_devices[] =
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0",
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001",
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
-    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p6",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p7",
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p25",
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p23",
-    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p21",
 
     "/sys/bus/mmc",
     "/sys/bus/mmc/drivers/mmcblk",
@@ -26,6 +26,10 @@ const char *mr_init_devices[] =
 
     "/sys/class/input/input2",
     "/sys/class/input/input2/event2",
+    "/sys/class/input/input4",
+    "/sys/class/input/input4/event4",
+    "/sys/class/input/input3",
+    "/sys/class/input/input3/event3",
     "/sys/class/misc/uinput",
     "/sys/class/input/input0",
     "/sys/class/input/input0/event0",
@@ -34,14 +38,14 @@ const char *mr_init_devices[] =
 
     // for adb
     "/sys/class/tty/ptmx",
-    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p21", //system
-    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p22", // /cache
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p23", //system
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p24", // /cache
     "/sys/class/misc/android_adb",
     "/sys/class/android_usb/android0/f_adb",
     "/sys/bus/usb",
 
     // Mount persist and firmware
-    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0:0001/block/mmcblk0/mmcblk0p20", //persist
+    "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0:0001/block/mmcblk0/mmcblk0p26", //persist
     "/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0:0001/block/mmcblk0/mmcblk0p1", //firmware
 
     // USB Drive is in here
